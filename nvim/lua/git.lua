cd5a6d7cd51de48be79be2db59614815127a22c9
Mohan Raj <praem1990@gmail.com>
2025-02-25 22:22:56 +0530

    Cleanup git.lua

--- nvim/lua/git.lua
+++ nvim/lua/git.lua
@@ -4,59 +4,70 @@ local M = {}
 
 M.title = "Git"
 
-M.push = function ()
-    local notification_record = vim.notify("Pushing...", vim.log.levels.INFO, {title = M.title})
+M.push = function (opts)
+    opts.args = {"push"}
+    opts.messages = vim.tbl_extend('keep', {
+        processing = "Pushing...",
+        success = "Pushed",
+        error = "Unable to push",
+    }, opts.messages or {})
+
+    M.exec(opts)
+end
+
+M.pull = function (opts)
+    opts.args = {"pull"}
+    opts.messages = vim.tbl_extend('keep', {
+        processing = "Pulling...",
+        success = "Pulled",
+        error = "Unable to pull",
+    }, opts.messages or {})
+
+    M.exec(opts)
+end
+
+M.fetch = function (opts)
+    opts.args = {"fetch"}
+    opts.messages = vim.tbl_extend('keep', {
+        processing = "Fetching...",
+        success = "Fetched",
+        error = "Unable to fetch",
+    }, opts.messages or {})
+
+    M.exec(opts)
+end
+
+M.exec = function (opts)
+    opts = opts or {}
+    local notify_opts = vim.tbl_extend("keep", {title = M.title }, opts.notify_opts or {})
+    notify_opts.replace = vim.notify(opts.messages.processing, vim.log.levels.INFO, notify_opts)
     Job:new({
         command = 'git',
-        args = { 'push' },
+        args = opts.args,
         cwd = vim.fn.getcwd(),
-        on_exit = vim.schedule_wrap( function(j, return_val)
+        on_exit = vim.schedule_wrap( function(_, return_val)
             if return_val == 0 then
-                vim.notify( "Pushed to remote", vim.log.levels.INFO, {title = M.title, replace = notification_record})
+                notify_opts.icon = ""
+                vim.notify(opts.messages.success, vim.log.levels.INFO, notify_opts)
             else
-                vim.notify( "Unable to push", vim.log.levels.ERROR, {title = M.title, replace = notification_record})
+                notify_opts.icon = ""
+                vim.notify(opts.messages.error, vim.log.levels.ERROR, notify_opts)
             end
         end),
     }):start()
 end
 
-M.pull = function ()
-    local notification_record = vim.notify("Pulling...", vim.log.levels.INFO, {title = M.title})
-    Job:new({
-        command = 'git',
-        args = { 'pull' },
-        cwd = vim.fn.getcwd(),
-        on_exit = vim.schedule_wrap( function(j, return_val)
-            if return_val == 0 then
-                vim.notify( "Pulled", vim.log.levels.INFO, {title=M.title, replace=notification_record})
-            else
-                vim.notify( "Unable to pull", vim.log.levels.ERROR, {title=M.title, replace=notification_record})
-            end
-        end),
-    }):start()
-end
-
-M.fetch = function ()
-    local notify_opts = {title = M.title}
-    notify_opts.replace = vim.notify("Fetching...", vim.log.levels.INFO, notify_opts)
-    Job:new({
-        command = 'git',
-        args = { 'fetch' },
-        cwd = vim.fn.getcwd(),
-        on_exit = vim.schedule_wrap( function(j, return_val)
-            if return_val == 0 then
-                vim.notify( "Fetched", vim.log.levels.INFO, notify_opts)
-            else
-                vim.notify( "Unable to fetch", vim.log.levels.ERROR, notify_opts)
-            end
-        end),
-    }):start()
-end
-
-M.setup = function ()
-    vim.keymap.set("n", "<leader>gp", M.push)
-    vim.keymap.set("n", "<leader>gl", M.pull)
-    vim.keymap.set("n", "<leader>gch", M.fetch)
+M.setup = function (opts)
+    opts = opts or {}
+    vim.keymap.set("n", "<leader>gp", function ()
+        M.push(opts)
+    end)
+    vim.keymap.set("n", "<leader>gl", function ()
+       M.pull(opts)
+    end)
+    vim.keymap.set("n", "<leader>gch", function ()
+       M.fetch(opts)
+    end)
 end
 
 return M
